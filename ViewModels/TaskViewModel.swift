732d653bd6bd7b28300adc7cc0d5f705857d7bc4
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2024-01-16 23:50:22 -0300

    create TaskViewModel

---
+++ ViewModels/TaskViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  TaskViewModel.swift
+//  TasksApp
+//
+//  Created by Kaique Lopes de Oliveira on 16/01/24.
+//
+
+import Foundation
+
+// MARK: - Estrutura de visualizacao de dados
+
+struct TaskViewModel {
+    private var task: Task
+    
+    init(task: Task) {
+        self.task = task
+    }
+    
+    var id: UUID {
+        task.id ?? UUID()
+    }
+    
+    var name: String {
+        task.name ?? ""
+    }
+    
+    var dueOn: Date {
+        task.dueOn ?? Date()
+    }
+    
+    var completedOn: Date? {
+        task.completedOn ?? Date()
+    }
+    
+    var completed: Bool {
+        task.completed
+    }
+}

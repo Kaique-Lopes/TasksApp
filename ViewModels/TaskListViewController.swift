a7e3ad21e288d28793287b08dc31059c185ae707
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2024-04-04 23:43:33 -0300

    create TaskListViewController

---
+++ ViewModels/TaskListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TaskListViewController.swift
+//  TasksApp
+//
+//  Created by Kaique Lopes de Oliveira on 04/04/24.
+//
+
+import Foundation
+
+class TaskListViewController {
+    var tasks = [TaskViewModel]()
+    
+    init() {
+        getAll()
+    }
+    
+    var numberOfTasks: Int {
+        tasks.count
+    }
+    
+    func getAll() {
+        // data from coreData and populate tasks
+    }
+    
+    func numberOfRows(by section: Int) -> Int {
+        if section == 0 {
+            return 1
+        }
+        
+        return numberOfTasks
+    }
+    
+    func getTasksByType() -> (completed: Int, incomplete: Int) {
+        let completeCount = tasks.lazy.filter({ $0.completed}).count
+        let incompleteCount = tasks.lazy.filter({!$0.completed}).count
+        
+        return (completeCount, incompleteCount)
+    }
+    
+    func task(by index: Int) -> TaskViewModel {
+        tasks[index]
+    }
+}

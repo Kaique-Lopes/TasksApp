0b9a5e291a823fd07e247212a68c081799356aa2
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2024-04-06 15:09:28 -0300

    Create context and SaveContext

--- Resources/CoreDataManager.swift
+++ Resources/CoreDataManager.swift
@@ -15,6 +15,26 @@ class CoreDataManager {
     
     lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "Tasks")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Error inexperado \(error), \(error.userInfo) ")
+            }
+        }
         return container
     }()
+    
+    var context: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+    func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                print("Error ao salvar \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 }

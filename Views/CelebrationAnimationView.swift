89d2017c04f78838e7343c967dd7c45e7acf745a
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2024-04-16 22:09:08 -0300

    create setupView and setupConstaints

--- Views/CelebrationAnimationView.swift
+++ Views/CelebrationAnimationView.swift
@@ -16,10 +16,33 @@ class CelebrationAnimationView: UIView {
         let animation = LottieAnimation.named(fileName)
         self.animationView = LottieAnimationView(animation: animation)
         super.init(frame: .zero)
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.contentMode = .scaleToFill
+        addSubview(animationView)
+        
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            animationView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            animationView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            animationView.topAnchor.constraint(equalTo: topAnchor),
+            animationView.bottomAnchor.constraint(equalTo: bottomAnchor)
+
+        ])
+    }
+    
+    func play(complete: @escaping (Bool) -> Void) {
+        animationView.play(completion: complete)
     }
     
 }

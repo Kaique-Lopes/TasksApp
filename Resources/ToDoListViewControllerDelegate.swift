263302fa63e1150edd19343854da8df50ff382f5
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2024-05-19 12:16:41 -0300

    create ToDoListViewControllerDelegate

---
+++ Resources/ToDoListViewControllerDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  ToDoListViewControllerDelegate.swift
+//  TasksApp
+//
+//  Created by Kaique Lopes de Oliveira on 19/05/24.
+//
+
+import UIKit
+
+extension ToDoListViewController: UITableViewDelegate {
+    
+}
